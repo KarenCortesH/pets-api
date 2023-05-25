8c9d6b8d659b8b60af9ca2e54a4e16c4f0a84b7a
Karen Cortes <karencortes9828@gmail.com>
2023-05-25 18:23:49 -0500

    feat: endpoints read and update for Pet

--- src/controllers/pet.controllers.ts
+++ src/controllers/pet.controllers.ts
@@ -1,34 +1,67 @@
 import { Request, Response } from "express";
 import { Pet } from "../entities/Pet";
+import { error } from "console";
 
 export const createPet = async (req: Request, res: Response) => {
-try {
-    const { fullName, breed, gender, color } = req.body;
-    const pet = new Pet();
-    pet.fullName = fullName;
-    pet.breed = breed;
-    pet.gender = gender;
-    pet.color = color;
+    try {
+        const { fullName, breed, gender, color } = req.body;
+        const pet = new Pet();
+        pet.fullName = fullName;
+        pet.breed = breed;
+        pet.gender = gender;
+        pet.color = color;
 
-    console.log(pet);
-    const savePet = await pet.save();
-    return savePet;
-} catch (error) {
-    if (error instanceof Error) {
-    return res.status(500).json({ message: error.message });
+        console.log(pet);
+        const savePet = await pet.save();
+        return savePet;
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
-}
 };
 
 export const deletePet = async (req: Request, res: Response) => {
-try {
-    const { id } = req.params;
-    const result = await Pet.delete({ id: parseInt(req.params.id) });
-    console.log(result);
-    if (result.affected === 0) return res.sendStatus(204);
-} catch (error) {
-    if (error instanceof Error) {
-    return res.status(500).json({ message: error.message });
+    try {
+        const { id } = req.params;
+        const result = await Pet.delete({ id: parseInt(req.params.id) });
+        console.log(result);
+        if (result.affected === 0) return res.sendStatus(204);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
     }
-}
+};
+
+export const updatePet = async (req: Request, res: Response) => {
+    try {
+        const { fullName, breed, gender, color } = req.body;
+        const pet = await Pet.findOneBy({ id: parseInt(req.params.id) });
+        if (!pet) return res.status(404).json({ message: "Pet does not exits" });
+        pet.fullName = fullName;
+        pet.breed = breed;
+        pet.gender = gender;
+        pet.color = color;
+
+        //Aqui guardo los cambios
+        pet.save();
+        console.log(pet);
+        return res.json("Received");
+    } catch (error) { }
+    if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const getPets = async (req: Request, res: Response) => {
+    try {
+        //buscamos todos los datos de la base de datos
+        const pets = await Pet.find();
+        return res.json(pets);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,21 +1,27 @@
 import { Router } from "express";
-import { createUser , deleteUser, updateUser} from "../controllers/user.controllers";
-import { createPet, deletePet } from "../controllers/pet.controllers";
+import { createUser, deleteUser, updateUser } from "../controllers/user.controllers";
+import { createPet, deletePet, getPets, updatePet } from "../controllers/pet.controllers";
 
 const router = Router();
 //devuelve este mensaje cuando visiten la ruta
-router.get("/hi",(req, res) => res.send("Hi World"));
+router.get("/hi", (req, res) => res.send("Hi World"));
 
-//router.post('/users');
+//USER//
 //route for create user
 router.post('/users', createUser);
 //route update user
-router.patch ('/users/:id', updateUser);
+router.patch('/users/:id', updateUser);
 //route delete user
-router.delete('/users/:id',deleteUser);
+router.delete('/users/:id', deleteUser);
 
+//PET//
 //route for create Pet
 router.post('/pet', createPet);
+//route for update Pet
+router.patch('/pet/:id', updatePet);
+//route for Read Pet
+router.get('/pet', getPets);
 //route for delete Pet
 router.delete('/pet/:id', deletePet);
+
 export default router
